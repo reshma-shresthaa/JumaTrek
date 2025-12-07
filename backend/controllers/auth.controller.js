bf2d9abcbad8d61157dff08fa5b83e6bd59662a0
Reshma-Shrestha777 <shrestha.reshma777@gmail.com>
2025-12-07 13:16:22 +0545

    Feat: User login API implemented

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -24,3 +24,27 @@ export const signUp=async (req,res) => {
         return res.status(500).json({message:`signup error ${error}`})
     }
 }
+
+export const login = async (req,res) => {
+    try {
+        let {email,password} = req.body
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"User doesn't exist!"})
+        }
+        let isMatch = await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({message:"Sorry! Incorrect Password. Please Try Again"})
+        }
+        let token = await genToken(user._id)
+        res.cookie("token",token,{
+            httpOnly:true,
+            secure:process.env.NODE_ENVIRONMENT = "production",
+            sameSite: "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        return res.status(201).json(user)
+    } catch (error) {
+        return res.status(500).json({message:`login error ${error}`})
+    }
+}
